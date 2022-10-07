24ab63fcc852986f7cf2e8cdea5dea5904fccf74
oceangravity <elefanteblanco5@gmail.com>
2022-10-07 18:43:12 -0400

    :)

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,40 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    AutoImport({
+      imports: [
+        "vue",
+        "vue-router",
+        "@vueuse/head",
+        "pinia",
+        {
+          "@/store": ["useStore"],
+        },
+      ],
+      dts: "src/auto-imports.d.ts",
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+    Components({
+      dirs: ["src/components"],
+      extensions: ["vue"],
+    }),
+    vue(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
-    port: 5183
-  }
-})
+    port: 5183,
+  },
+});
