abc2256c94d9b91cf14a1f5b82a7d5cef93ffc6b
oceangravity <elefanteblanco5@gmail.com>
2022-10-11 08:49:04 -0400

    :)

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,22 +1,7 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
-  <div>
-    <header .$WebsKit="{ guid: 1}">
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-      <div class="wrapper">
-        <nav .$WebsKit="{ guid: 2}">
-          <RouterLink to="/home">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div>
-    </header>
-
-    <RouterView />
-
-    <p .$WebsKit="{ guid: 3}">MI#ERDA TIESA</p>
+  <div .$WebsKit="{ guid: 0 }" .guid="0">
+    <div .$WebsKit="{ guid: 1 }" .guid="1" class="p-4 bg-blue-500"></div>
+    <div .$WebsKit="{ guid: 2 }" .guid="2" class="p-4 bg-red-500"></div>
   </div>
 </template>
+
