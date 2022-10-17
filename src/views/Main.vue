c70e3dab4b50986bef049cb9d36d1061f28132cc
oceangravity <elefanteblanco5@gmail.com>
2022-10-16 20:09:22 -0400

    :)

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,3 +1,3 @@
-<template><div class="p-4 bg-blue-500" .$wk="{ guid: '1', parent: '0' }"></div>
-<div class="p-4 bg-yellow-500" .$wk="{ guid: '3', parent: '0' }"></div>
-<div class="p-4 bg-red-500" .$wk="{ guid: '2', parent: '0' }"></div></template>
+<template><div class="p-4 bg-yellow-500" .$wk="{ guid: '3', parent: '0' }"></div>
+<div class="p-4 bg-red-500" .$wk="{ guid: '2', parent: '0' }"></div>
+<div class="p-4 bg-blue-500" .$wk="{ guid: '1', parent: '0' }"></div></template>
