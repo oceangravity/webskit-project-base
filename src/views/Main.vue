8d0de3b995c1f7ecf3bddb912792eec73b29d420
oceangravity <elefanteblanco5@gmail.com>
2022-10-22 11:29:13 -0400

    :)

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,7 +1,8 @@
 <template>
-    <div .$wk="{ guid: 1, isContainer: true, parent: 0 }" class="p-4 bg-blue-500">
-      <div .$wk="{ guid: 3, isContainer: false, parent: 1 }" class="p-4 bg-yellow-50"></div>
-    </div>
-    <div .$wk="{ guid: 2, isContainer: true, parent: 0 }" class="p-4 bg-red-500"></div>
+  <div .$wk="{ guid: 1, isContainer: true, parent: 0 }" class="p-4 bg-blue-500">
+    <div .$wk="{ guid: 3, isContainer: false, parent: 1 }" class="p-4 bg-yellow-50"></div>
+  </div>
+  <div .$wk="{ guid: 2, isContainer: true, parent: 0 }" class="p-4 bg-red-500">
+    <normal-component></normal-component>
+  </div>
 </template>
-
---
+++ src/views/NormalComponent.vue
@@ -0,0 +1,6 @@
+<template>
+    <div .$wk="{ guid: 5, isContainer: false, parent: 4 }">
+      <div v-for="item in 10" .$wk="{ guid: 6, isContainer: false, parent: 5 }">{{ item }}</div>
+    </div>
+</template>
+
